b5c9fe1e5d6bdd5ff7bc3ab2f474f1ec5158f209
Chris <cmjoy136@gmail.com>
2019-10-18 09:11:57 -0400

    fixed  issue where old inefficient function was still being called and tracked by the timeit function. More efficient function should be tracked and shows improvement in time taken.

--- tuneup.py
+++ tuneup.py
@@ -48,22 +48,11 @@ def new_and_improved_moviefinder(src):
            duplicates.append(mov)
     return duplicates
 
-@profile
-def find_duplicate_movies(src):
-    """Returns a list of duplicate movies from a src list"""
-    movies = read_movies(src)
-    duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
-            duplicates.append(movie)
-    return duplicates
-
 def timeit_helper(reps, runs_per_rep):
     """Part A:  Obtain some profiling measurements using timeit"""
     # YOUR CODE GOES HERE
-    t = timeit.Timer(stmt="find_duplicate_movies('movies.txt')",
-                     setup="from __main__ import find_duplicate_movies")
+    t = timeit.Timer(stmt="new_and_improved_moviefinder('movies.txt')",
+                     setup="from __main__ import new_and_improved_moviefinder")
     result = t.repeat(reps, runs_per_rep)
     best_time = min(result)/float(runs_per_rep)
     print('Best time across {} repeats of {} runs per repeat: {} sec'.format(
@@ -72,12 +61,9 @@ def timeit_helper(reps, runs_per_rep):
 def main():
     """Computes a list of duplicate movie entries"""
     timeit_helper(7, 5)
-    result = find_duplicate_movies('movies.txt')
-    result2 = new_and_improved_moviefinder('movies.txt')
+    result = new_and_improved_moviefinder('movies.txt')
     print('Found {} duplicate movies:'.format(len(result)))
-    print('Found {} duplicate movies:'.format(len(result2)))
     print('\n'.join(result))
-    print('\n'.join(result2))
 
 if __name__ == '__main__':
     main()
